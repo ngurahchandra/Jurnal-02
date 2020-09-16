5e9135387aa4120c7c0b722e75e267b037000185
ngurahchandra <ngurahchandra19@gmail.com>
2020-09-16 18:30:03 +0800

    HelloWorld.java

---
+++ Praktikum 2/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Nama : Si Gede Ngurah Chandra Adi Natha");
+		System.out.println("Nim : 6706200059");
+		System.out.println("Kelas : D3RPLA 44-04");
+	}
+}
